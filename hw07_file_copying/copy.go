6ede5bb75a83cb9f12713b0ef16ccb96334ad6c1
Andrzej Makaś <a.makas@riot.agency>
2024-10-05 09:55:33 +0200

    HW07 is completed

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -2,6 +2,11 @@ package main
 
 import (
 	"errors"
+	"io"
+	"io/fs"
+	"os"
+
+	"github.com/cheggaaa/pb/v3"
 )
 
 var (
@@ -10,6 +15,59 @@ var (
 )
 
 func Copy(fromPath, toPath string, offset, limit int64) error {
-	// Place your code here.
+	bytes, err := validateFile(fromPath, offset, limit)
+	if err != nil {
+		return err
+	}
+
+	fromFile, err := os.Open(fromPath)
+	if err != nil {
+		return err
+	}
+	defer fromFile.Close()
+	if offset > 0 {
+		fromFile.Seek(offset, 0)
+	}
+
+	toFile, err := os.Create(toPath)
+	if err != nil {
+		return err
+	}
+	defer toFile.Close()
+
+	bar := pb.Full.Start64(bytes)
+	toFileWriter := bar.NewProxyWriter(toFile)
+	defer bar.Finish()
+
+	_, err = io.CopyN(toFileWriter, fromFile, bytes)
+	if err != nil {
+		return err
+	}
+
 	return nil
+}
+
+func validateFile(name string, offset, limit int64) (int64, error) {
+	info, err := os.Stat(name)
+	if err != nil {
+		if os.IsNotExist(err) {
+			return 0, fs.ErrNotExist
+		}
+		return 0, err
+	}
+	if isUnsupportedFile(info.Mode()) {
+		return 0, ErrUnsupportedFile
+	}
+	if offset > info.Size() {
+		return 0, ErrOffsetExceedsFileSize
+	}
+	bytes := info.Size() - offset
+	if limit > 0 && bytes > limit {
+		bytes = limit
+	}
+	return bytes, nil
+}
+
+func isUnsupportedFile(m fs.FileMode) bool {
+	return m&os.ModeDevice != 0 || m&os.ModeCharDevice != 0 || m&os.ModeDir != 0 || m&os.ModeSocket != 0
 }
--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -1,7 +1,73 @@
 package main
 
-import "testing"
+import (
+	"fmt"
+	"io/fs"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+var (
+	tempPath      = "/tmp/"
+	tempExtension = "*.txt"
+)
 
 func TestCopy(t *testing.T) {
-	// Place your code here.
+	for _, test := range []struct {
+		offset int64
+		limit  int64
+		file   string
+	}{
+		{offset: 0, limit: 0, file: "testdata/out_offset0_limit0.txt"},
+		{offset: 0, limit: 10, file: "testdata/out_offset0_limit10.txt"},
+		{offset: 0, limit: 1000, file: "testdata/out_offset0_limit1000.txt"},
+		{offset: 0, limit: 10000, file: "testdata/out_offset0_limit10000.txt"},
+		{offset: 100, limit: 1000, file: "testdata/out_offset100_limit1000.txt"},
+		{offset: 6000, limit: 1000, file: "testdata/out_offset6000_limit1000.txt"},
+	} {
+		t.Run(fmt.Sprintf("Offset:%d Limit:%d", test.offset, test.limit), func(t *testing.T) {
+			toFile, _ := os.CreateTemp(tempPath, tempExtension)
+			defer os.Remove(toFile.Name())
+
+			err := Copy("testdata/input.txt", toFile.Name(), test.offset, test.limit)
+			require.NoError(t, err)
+
+			fromFile, _ := os.Open(test.file)
+			defer fromFile.Close()
+
+			fromFileContent, _ := os.ReadFile(fromFile.Name())
+			toFileContent, _ := os.ReadFile(toFile.Name())
+			require.Equal(t, fromFileContent, toFileContent)
+		})
+	}
+
+	t.Run("Error: not existed file", func(t *testing.T) {
+		err := Copy("input.txt", "output.txt", 0, 0)
+		require.Error(t, err)
+		require.ErrorIs(t, err, fs.ErrNotExist)
+	})
+
+	t.Run("Error: file is directory", func(t *testing.T) {
+		err := Copy(tempPath, tempPath+"output.txt", 0, 0)
+		require.Error(t, err)
+		require.ErrorIs(t, err, ErrUnsupportedFile)
+	})
+
+	t.Run("Error: device file", func(t *testing.T) {
+		toFile, _ := os.CreateTemp(tempPath, tempExtension)
+		defer os.Remove(toFile.Name())
+
+		err := Copy("/dev/urandom", toFile.Name(), 0, 0)
+		require.EqualError(t, err, ErrUnsupportedFile.Error())
+	})
+
+	t.Run("Error: offset more then size", func(t *testing.T) {
+		toFile, _ := os.CreateTemp(tempPath, tempExtension)
+		defer os.Remove(toFile.Name())
+
+		err := Copy(toFile.Name(), tempPath+"output.txt", 1000, 0)
+		require.Equal(t, err, ErrOffsetExceedsFileSize)
+	})
 }
